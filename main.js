b4caab0526c2a64136207ecd5e4dd419f75b47bc
Andreas981 <amikal@hotmail.no>
2022-03-19 21:37:39 +0100

    First working version

--- main.js
+++ main.js
@@ -1,16 +1,32 @@
+let roundStarted = false;
+let currentName = "";
+
 const startRound = async () => {
-  // Fetch image
-  // Fetch name
+  if (roundStarted) return;
+  roundStarted = true;
   let fullNumber = getRandomNumber();
+
+  currentNumber = fullNumber;
 
   let gifName = fullNumber + ".gif";
   let name = pokemonList[fullNumber].name;
+  currentName = name;
+
+  console.log(name);
 
   playAudio();
+  showTitleImage();
   await delay(2500);
   showBlurredImage(gifName);
+};
 
-  return gifName;
+const checkName = async (name) => {
+  let correctName = name.toLowerCase().replaceAll(/\s/g, "");
+  if (correctName === currentName) {
+    document.getElementById("pokemon-image").className = "";
+    await delay(4000);
+    stopRound();
+  }
 };
 
 const delay = (ms) => new Promise((res) => setTimeout(res, ms));
@@ -23,6 +39,21 @@ function showBlurredImage(gifName) {
 
   let parentElement = document.getElementById("pokemon-holder");
   parentElement.appendChild(blurredImage);
+}
+
+function stopRound() {
+  document.getElementById("pokemon-image").remove();
+  hideTitleImage();
+  currentName = "";
+  roundStarted = false;
+}
+
+function showTitleImage() {
+  document.getElementById("title-text").style = "display: block;";
+}
+
+function hideTitleImage() {
+  document.getElementById("title-text").style = "display: none;";
 }
 
 function playAudio() {
