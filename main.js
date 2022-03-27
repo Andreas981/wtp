4bfed67b2d8632a5dbebd9ea92e5b4102452d774
Andreas981 <32385759+Andreas981@users.noreply.github.com>
2022-03-27 14:49:57 +0200

    Added new version

--- main.js
+++ main.js
@@ -1,63 +1,48 @@
-let roundStarted = false;
+let holder = document.getElementById("holder");
+let ball = document.getElementById("ball");
+let title = document.getElementById("title");
+let wellDone = document.getElementById("wellDone");
+let ballVid = document.getElementById("ballVid");
+let mute = false;
+let auto = true;
+let per = "All";
+let language = "en";
+let channel = "AsyncBit";
+let botuser = "";
+let token = "oauth:gg3u8u0mu3kjc2au4v8rd580hl5uas";
+let pokemon;
+let pokeDex = "true";
+let heightTxt = "Height";
+let weightTxt = "Weight";
+let foundByTxt = "Found By";
+let wellDoneTxt = "Well Done";
+let wasRightTxt = "You Was Right";
+let titleTxt = "Who's that Pokémon?";
+
+let isSolved = false;
+let min = 1;
+let max = 151;
+
+let resource;
+let ext;
+let rType = "animated";
+let cry = true;
+let pokemonID;
+let tShow = true;
+
+// Mine
+let pokeNum = 0;
 let currentName = "";
 
-const startRound = async () => {
-  if (roundStarted) return;
-  roundStarted = true;
-  let fullNumber = getRandomNumber();
-
-  currentNumber = fullNumber;
-
-  let gifName = fullNumber + ".gif";
-  let name = pokemonList[fullNumber].name;
-  currentName = name;
-
-  console.log(name);
-
-  playAudio();
-  showTitleImage();
-  await delay(2500);
-  showBlurredImage(gifName);
-};
-
-const checkName = async (name) => {
-  let correctName = name.toLowerCase().replaceAll(/\s/g, "");
-  if (correctName === currentName) {
-    document.getElementById("pokemon-image").className = "";
-    await delay(4000);
-    stopRound();
-  }
-};
-
-const delay = (ms) => new Promise((res) => setTimeout(res, ms));
-
-function showBlurredImage(gifName) {
-  let blurredImage = document.createElement("img");
-  blurredImage.src = "./assets/" + gifName;
-  blurredImage.className = "blurred";
-  blurredImage.id = "pokemon-image";
-
-  let parentElement = document.getElementById("pokemon-holder");
-  parentElement.appendChild(blurredImage);
-}
-
-function stopRound() {
-  document.getElementById("pokemon-image").remove();
-  hideTitleImage();
-  currentName = "";
-  roundStarted = false;
-}
-
-function showTitleImage() {
-  document.getElementById("title-text").style = "display: block;";
-}
-
-function hideTitleImage() {
-  document.getElementById("title-text").style = "display: none;";
-}
-
-function playAudio() {
-  document.getElementById("who-that-pokemon-audio").play();
+if (botuser) {
+  ComfyJS.Init(botuser, token, channel);
+  console.log("Bot Send");
+} else if (token) {
+  ComfyJS.Init(channel, token);
+  console.log("Channel Send");
+} else {
+  ComfyJS.Init(channel);
+  console.log("No Send");
 }
 
 function getRandomNumber() {
@@ -74,4 +59,167 @@ function getAmoutOfZeros(number) {
   } else {
     return "";
   }
+}
+
+function startGame() {
+  isSolved = false;
+
+  if (rType === "original") {
+    resource = "original";
+    ext = "png";
+  } else {
+    resource = "animated";
+    ext = "gif";
+  }
+
+  pokeNum = getRandomNumber();
+
+  currentName = pokemonList[pokeNum].name;
+  console.log(currentName);
+
+  document.getElementById("ball").style.visibility = "visible";
+
+  document.getElementById("ballVid").play();
+  if (!mute) {
+    document.getElementById("who-that-pokemon-audio").play();
+  }
+}
+
+function stopVid() {
+  document.getElementById("ball").style.visibility = "hidden";
+  showBlurredImage();
+  console.log("HIDE");
+}
+
+function showBlurredImage() {
+  let blurredImage = document.createElement("img");
+  blurredImage.src = "./assets/" + pokeNum + ".gif";
+  blurredImage.className = "blurred";
+  blurredImage.id = "pokemon-image";
+
+  let parentElement = document.getElementById("pokemon-holder");
+  parentElement.appendChild(blurredImage);
+}
+
+function giveUp() {
+  guess(currentName);
+}
+
+function skip() {
+  winReset();
+  startGame();
+}
+
+// message, user
+function guess(x, n) {
+  console.log(x);
+  console.log(n);
+  let guessedName = x.toLowerCase().replaceAll(/\s/g, "");
+  if (guessedName == currentName) {
+    isSolved = true;
+    currentName = "";
+    document.getElementById("pokemon-image").className = "";
+    ComfyJS.Say("Congratz " + n + ". You guessed that it was " + guessedName);
+    setTimeout(function () {
+      winReset();
+    }, 5000);
+  }
+}
+
+ComfyJS.onCommand = (user, command, message, flags, extra) => {
+  if (
+    (flags.broadcaster && command === "wtp") ||
+    (flags.mod && command === "wtp")
+  ) {
+    if (!isSolved) {
+      startGame();
+    } else {
+      skip();
+    }
+  }
+  if (
+    (flags.broadcaster && command === "resetwtp") ||
+    (flags.mod && command === "resetwtp")
+  ) {
+    winReset();
+  }
+  if (
+    (flags.broadcaster && command === "skipwtp") ||
+    (flags.mod && command === "skipwtp")
+  ) {
+    skip();
+  }
+  if (
+    (flags.broadcaster && command === "giveup") ||
+    (flags.mod && command === "giveup")
+  ) {
+    giveUp();
+  }
+  if (
+    (flags.broadcaster && command === "stopwtp") ||
+    (flags.mod && command === "stopwtp")
+  ) {
+    stopGame();
+  }
+  if (
+    (flags.broadcaster && command === "stopwtpauto") ||
+    (flags.mod && command === "stopwtpauto")
+  ) {
+    stopAuto();
+  }
+  if (
+    (flags.broadcaster && command === "startwtpauto") ||
+    (flags.mod && command === "startwtpauto")
+  ) {
+    startAuto();
+  }
+};
+
+ComfyJS.onChat = (user, message, flags, self, extra) => {
+  if (!isSolved) {
+    message = message.replace("?", "");
+    message = message.replace("@", "");
+    message = message.split(" ");
+
+    if (per === "All") {
+      guess(message[0].toLowerCase(), user);
+    } else if (per === "Subs") {
+      if (flags.subscriber || flags.mod || flags.broadcaster) {
+        guess(message[0].toLowerCase(), user);
+      }
+    } else if (per === "Vips" || flags.mod || flags.broadcaster) {
+      if (flags.vip) {
+        guess(message[0].toLowerCase(), user);
+      }
+    } else if (per === "Vip/Subs") {
+      if (flags.vip || flags.subscriber || flags.mod || flags.broadcaster) {
+        guess(message[0].toLowerCase(), user);
+      }
+    }
+  }
+};
+
+function winReset() {
+  isSolved = false;
+  document.getElementById("pokemon-image").remove();
+}
+
+function stopGame() {
+  location.reload();
+}
+
+let autoStart = false;
+
+if (autoStart) {
+  startGame();
+}
+
+function stopAuto() {
+  auto = false;
+  winReset();
+}
+
+function startAuto() {
+  auto = true;
+  startGame();
 }
